c5f18cba94ac5cf8a997a2bee9d52f48de49203a
vahagsaribekyan <32436397+vahagsaribekyan@users.noreply.github.com>
2019-05-05 11:09:15 +0400

    test

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('JavaScript HTTP trigger functionnnnnnnnnnnnn processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
